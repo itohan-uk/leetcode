154f710d76820bcfea4a4fdda1def047301b6390
Itohan <itohan.ukponmwan@intel.com>
2019-12-19 14:55:13 -0800

    Sqrt binary search question

---
+++ sqrt.py
@@ -0,0 +1,45 @@
+'''
+Leetcode binary search question 
+Implement int sqrt(int x).
+
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
+Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+
+Example 1:
+
+Input: 4
+Output: 2
+Example 2:
+
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since 
+             the decimal part is truncated, 2 is returned.
+'''
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+       
+        bg = 0
+        ed = x
+        
+        prev = 0
+        
+        while bg <= ed:
+            mid = (ed + bg) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                ed = mid - 1
+                if ed * ed < x:
+                    return ed
+            elif mid * mid < x:
+                bg = mid + 1
+        
+
+        return -1
